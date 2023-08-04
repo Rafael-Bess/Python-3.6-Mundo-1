33dc8d416a77bee36c69389e20dc4dff26558371
Rafael Bess <140470059+Rafael-Bess@users.noreply.github.com>
2023-08-04 02:45:31 -0300

    Bess

--- pythonProject/Aula 06/Aula06.py
+++ pythonProject/Aula 06/Aula06.py
@@ -5,3 +5,4 @@ print('A soma entre {} e {} Vale {}'.format(n1,n2,r))'''
 
 
 print ('hello world!!')
+print('hello world!!')
--- pythonProject/Fase12/Exercicio37.py
+++ pythonProject/Fase12/Exercicio37.py
@@ -1,1 +1,6 @@
-Exercicio36.py
+'''Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão
+1 para binário
+2 para Octal
+3 para hexadecimal
+'''
+
