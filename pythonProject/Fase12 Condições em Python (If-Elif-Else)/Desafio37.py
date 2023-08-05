41c3eec31c9616ff88f113c25003ce845d31aa3a
Rafael Bess <140470059+Rafael-Bess@users.noreply.github.com>
2023-08-05 19:36:14 -0300

    a

--- pythonProject/Fase12 Condições em Python (If-Elif-Else)/Desafio37.py
+++ pythonProject/Fase12 Condições em Python (If-Elif-Else)/Desafio37.py
@@ -3,4 +3,17 @@
 2 para Octal
 3 para hexadecimal
 '''
-
+num = int(input('Insira um número qualquer: '))
+forma = int(input('Qual será a forma de conversão utilizada?\n'
+                  '1 - Conversão para Binário\n'
+                  '2 - Conversão para Octal\n'
+                  '3 - Conversão para Hexadecimal\n'
+                  'Digite a forma utilizada: '))
+if forma == 1:
+    print('O número {} convertido para Binário é {}'.format(num, bin(num)))
+elif forma == 2:
+    print('O número {} convertido para Octal é {}'.format(num, oct(num)))
+elif forma == 3:
+    print('O número {} convertido para Hexadecimal é {}'.format(num, hex(num)))
+else:
+    print('Forma não encontrada')
